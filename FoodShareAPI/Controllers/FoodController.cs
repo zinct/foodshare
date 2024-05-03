0033bd05e5d9011d1c54269872c046747774c722
Indra Mahesa <indramahesa128@gmail.com>
2024-05-03 21:59:37 +0700

    feat: update food model & refactoring code

---
+++ FoodShareAPI/Controllers/FoodController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FoodShareAPI.Models;
+
+namespace FoodShareAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FoodController : ControllerBase
+    {
+        public static List<Food> foodList = new List<Food>();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Food>> Get()
+        {
+            try
+            {
+                return foodList;
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500);
+            }
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<Food> Show(int id)
+        {
+            try
+            {
+                return foodList[id];
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Store(Food food)
+        {
+            foodList.Add(food);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                foodList.RemoveAt(id);
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                
+                return StatusCode(500);
+            }
+            
+        }
+    }
+}
+
