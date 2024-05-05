9b75f78424d749062595aa1a0b8ba17769980a42
Feeee <Fee@gmail.com>
2024-05-05 22:49:26 +0700

    feat: add date validator function

--- FoodShareCore/Utilities/DateUtilities.cs
+++ FoodShareCore/Utilities/DateUtilities.cs
@@ -4,7 +4,7 @@
     {
         //public enum DayOfWeekInEnglish { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };
         //public string[] DayOfWeekInIndonesian = { "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu" };
-        public static string ConvertIntToMonthString(int month)
+        internal static string ConvertIntToMonthString(int month)
         {
             String[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
             return months[month - 1];
@@ -16,6 +16,21 @@
             string month = ConvertIntToMonthString(Int32.Parse(date.Month.ToString()));
             string year = date.Year.ToString();
             return dayOfWeek + ", " + day + " " + month + " " + year;
+        }
+
+        //input 2069-02-02
+        public bool ValidateDateInput(String date)
+        {
+            if (date.Length > 10) return false;
+
+            int[] totalDayInAMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+            int year = int.Parse(date.Substring(0, 4));
+            int month = int.Parse(date.Substring(5, 2));
+            int day = int.Parse(date.Substring(8, 2));
+
+            bool isLeapYear = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+            
+            return year >= 0 && year <= 9999 && month >= 0 && month <= 12 && ((day >= 0 && day <= totalDayInAMonth[month-1]) || (isLeapYear && day >= 0 && day <= 29 ));  
         }
     }
 }
