6ef04add9611a091f34fe9815339a19940a8eced
hsilasss <hsilassamban@gmail.com>
2024-05-06 00:12:37 +0700

    menambahkan unit testing untuk masing2 utilities

---
+++ FoodShareUnitTesting/kategoritests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoodShareCore.Utilities;
+using static FoodShareCore.Utilities.tableDrivenKategori;
+
+namespace FoodShareUnitTesting
+{
+    [TestClass]
+    public class kategoritests
+    {
+        [TestMethod]
+        public void TestDaging()
+        {
+            string namaMakanan = "Daging";
+            Kategori value = tableDrivenKategori.Getkategori(namaMakanan);
+            Assert.AreEqual(Kategori.Daging, value);
+        }
+
+        [TestMethod]
+        public void TestAyam()
+        {
+            string namaMakanan = "Ayam";
+            Kategori value = tableDrivenKategori.Getkategori(namaMakanan);
+            Assert.AreEqual(Kategori.Ayam, value);
+        }
+
+        [TestMethod]
+        public void TestIkan()
+        {
+            string namaMakanan = "Ikan / Produk laut";
+            Kategori value = tableDrivenKategori.Getkategori(namaMakanan);
+            Assert.AreEqual(Kategori.Ikan, value);
+        }
+
+        [TestMethod]
+        public void TestSayuran()
+        {
+            string namaMakanan = "Sayuran";
+            Kategori value = tableDrivenKategori.Getkategori(namaMakanan);
+            Assert.AreEqual(Kategori.Sayuran, value);
+        }
+
+        [TestMethod]
+        public void TestBuah()
+        {
+            string namaMakanan = "Buah";
+            Kategori value = tableDrivenKategori.Getkategori(namaMakanan);
+            Assert.AreEqual(Kategori.Buah, value);
+        }
+
+        [TestMethod]
+        public void TestPerbijian()
+        {
+            string namaMakanan = "Biji-bijian";
+            Kategori value = tableDrivenKategori.Getkategori(namaMakanan);
+            Assert.AreEqual(Kategori.Perbijian, value);
+        }
+
+        [TestMethod]
+        public void TestSusu()
+        {
+            string namaMakanan = "Susu";
+            Kategori value = tableDrivenKategori.Getkategori(namaMakanan);
+            Assert.AreEqual(Kategori.Susu, value);
+        }
+
+        [TestMethod]
+        public void TestRotiKue()
+        {
+            string namaMakanan = "Roti / Kue";
+            Kategori value = tableDrivenKategori.Getkategori(namaMakanan);
+            Assert.AreEqual(Kategori.RotiKue, value);
+        }
+
+        [TestMethod]
+        public void TestCemilan()
+        {
+            string namaMakanan = "Cemilan";
+            Kategori value = tableDrivenKategori.Getkategori(namaMakanan);
+            Assert.AreEqual(Kategori.Cemilan, value);
+        }
+
+        [TestMethod]
+        public void TestInstan()
+        {
+            string namaMakanan = "Siap Saji";
+            Kategori value = tableDrivenKategori.Getkategori(namaMakanan);
+            Assert.AreEqual(Kategori.Instan, value);
+        }
+    }
+
+}
+
