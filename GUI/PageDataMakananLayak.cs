d6057280ca1737186a9833331ac7becbc527cf83
hsilasss <hsilassamban@gmail.com>
2024-06-11 03:05:53 +0700

    add page Makanan Layak

--- GUI/PageDataMakananLayak.cs
+++ GUI/PageDataMakananLayak.cs
@@ -16,5 +16,24 @@ namespace GUI
         {
             InitializeComponent();
         }
+
+        private void MakananKeluarGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PageDataMakananLayak_Load(object sender, EventArgs e)
+        {
+            MakananLayakGrid.Rows.Add("1", "Nasi Goreng", "12-12-2003", "12-12-2004", "1000");
+            MakananLayakGrid.Rows.Add("2", "Nasi Goreng", "12-12-2003", "12-12-2004", "1000");
+            MakananLayakGrid.Rows.Add("3", "Nasi Goreng", "12-12-2003", "12-12-2004", "1000");
+            MakananLayakGrid.Rows.Add("4", "Nasi Goreng", "12-12-2003", "12-12-2004", "1000");
+            MakananLayakGrid.Rows.Add("5", "Nasi Goreng", "12-12-2003", "12-12-2004", "1000");
+        }
     }
 }
